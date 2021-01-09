a163e6c511b94919b1d0fff163379c6944f6fb48
ndbellew <ndbellew@gmail.com>
2021-01-09 01:10:46 -0700

    Web Forms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
-
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +8,11 @@ def index():
     user = {"username":"Ndbellew"}
     posts = [{"author":{"username":"John"},"body":"Yeet!"}]
     return render_template('index.html', title="home", user=user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login request for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
