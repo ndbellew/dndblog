5150a0a91b1140233f9c9cb856651d76cb5c7e0e
ndbellew <ndbellew@gmail.com>
2021-01-12 21:41:10 -0700

    Profile Page and Avatars

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Integer, Column, String
 from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from hashlib import md5
 
 class User(UserMixin,db.Model):
     id = Column(Integer, primary_key=True)
@@ -11,6 +11,12 @@ class User(UserMixin,db.Model):
     email = Column(String(128), index=True, unique=True)
     password_hash = Column(String(128), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = Column(db.DateTime, default=datetime.utcnow)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
     def __repr__(self):
         return f'<User {self.username}>'
