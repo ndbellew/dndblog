f17bfc1bfc01dce3f63c8b04ecc538a74cb7193b
ndbellew <ndbellew@gmail.com>
2021-01-13 20:28:16 -0700

    Error Handling

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ class Config(object):
     MYSQL_DATABASE_DB = 'dndblog'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://DBMU:GameWinner!11@localhost/dndblog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['superdaggler@gmail.com']
