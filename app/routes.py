c67eb7209d78e765a8f5e9dd816b9ed9619a71f1
ndbellew <ndbellew@gmail.com>
2021-01-14 20:09:07 -0700

    Pagination Bug Fix

--- app/routes.py
+++ app/routes.py
@@ -64,14 +64,17 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
+    if username == current_user.username:
+        return redirect(url_for('index'))
     user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('user', username=user.username, page=posts.next_num) \
         if posts.has_next else None
     prev_url = url_for('user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form, next_url=next_url, prev_url=prev_url)
+    return render_template('user.html', user=user, posts=posts.items, form=form, next_url=next_url, prev_url=prev_url)
 
 @app.before_request
 def before_request():
